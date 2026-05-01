68bdc5a377ac64e5ba1bffbe18e0ea04868bde98
Shwankie <shwankie1@gmail.com>
2026-05-01 18:03:02 -0400

    Update2.0 main.py

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import os
 import asyncio
-import random
-
 import discord
 from dotenv import load_dotenv
-
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 SPOTIFYID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFYSECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 PLAYLISTID = os.getenv('SPOTIFY_PLAYLIST_ID')
+REFRESH_TOKEN = os.getenv('SPOTIFY_REFRESH_TOKEN')
+PLAYLIST_URL = f"https://open.spotify.com/playlist/{PLAYLISTID}"
 
-
-# Default intents are now required to pass to Client
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
-
 
 async def add_song_to_playlist(song_url, playlist_id):
     auth = SpotifyOAuth(
@@ -28,8 +25,7 @@ async def add_song_to_playlist(song_url, playlist_id):
         redirect_uri='http://127.0.0.1:8888/callback',
         scope='playlist-modify-public'
     )
-    
-    REFRESH_TOKEN = os.getenv('SPOTIFY_REFRESH_TOKEN')
+
     token_info = auth.refresh_access_token(REFRESH_TOKEN)
     sp = spotipy.Spotify(auth=token_info['access_token'])
 
@@ -46,7 +42,6 @@ async def add_song_to_playlist(song_url, playlist_id):
     print(track_name)
     return track_name
 
-
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
@@ -56,10 +51,25 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('https://open.spotify.com/track/'):
-        track_name = await add_song_to_playlist(message.content, PLAYLISTID)
-        embed = discord.Embed(title=f"Adding '" + track_name + "' to my playlist.", color=0x00ff00)
+    if message.content == '!playlist':
+        embed = discord.Embed(
+            title="Our Server Playlist 🎵",
+            description="Click below to open and add songs!",
+            color=0x1DB954
+        )
+        embed.add_field(
+            name="Open Playlist",
+            value=f"[Click here to open on Spotify]({PLAYLIST_URL})"
+        )
+        embed.set_footer(text="Add songs by pasting a Spotify track link in this channel")
         await message.channel.send(embed=embed)
 
+    if message.content.startswith('https://open.spotify.com/track/'):
+        track_name = await add_song_to_playlist(message.content, PLAYLISTID)
+        embed = discord.Embed(
+            title=f"Adding '{track_name}' to the playlist!",
+            color=0x1DB954
+        )
+        await message.channel.send(embed=embed)
 
 client.run(TOKEN)
